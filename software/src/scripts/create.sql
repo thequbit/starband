606c91c1fe8ae591f7a988edefbf5ff7f74b6a14
thequbit <thequbit@gmail.com>
2013-04-20 22:08:37 -0400

    some creation scripts

---
+++ software/src/scripts/create.sql
@@ -0,0 +1,39 @@
+create database starband;
+
+-- set permisions here
+
+create table stars(
+starid int not null auto_increment primary key,
+visiblemagnitude double not null,
+propername varchar(255) not null,
+simbad varchar(255) not null,
+wikilink text not null
+);
+
+create table users(
+userid int not null auto_increment primary key,
+username text not null,
+password text not null,
+displayname text not null
+);
+
+create table userbracelets(
+userbracelets int not null auto_increment primary key,
+userid int not null,
+foreign key (userid) references users(userid),
+braceletid int not null,
+foreign key (braceletid) references bracelets(braceletid),
+);
+
+create table bracelets(
+braceletid int not null auto_increment primary key,
+braceletname text not null
+);
+
+create table assignments(
+assignmentid int not null auto_increment primary key,
+braceletid int not null,
+foreign key (braceletid) references bracelets(braceletid),
+starid int not null,
+foreign key (starid) references stars(starid)
+);
